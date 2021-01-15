57892de98d8b83ae8b44b08cbd1bbcdd89aaa703
MD Ahasan Habib <ahasanh303@gmail.com>
2021-01-16 00:20:24 +0600

    creatig component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import './App.css';
 //Component
-import Navigation from './components/Navigation/Navigation';
+import Navigation from './components/Nav/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLink from './components/Input/ImageLink'
+import 'tachyons'
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
-      {/*<Logo/>
+      <Logo/>
       <ImageLink/>
-      <FaceRecognation/>*/}
+      {/*<FaceRecognation/>*/}
     </div>
   );
 }
