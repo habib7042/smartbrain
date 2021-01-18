de9db86d33db49df497aea8e3566202346f57fe7
MD Ahasan Habib <ahasanh303@gmail.com>
2021-01-19 00:42:10 +0600

    few css change

--- src/components/Input/ImageLink.js
+++ src/components/Input/ImageLink.js
@@ -4,12 +4,12 @@ import './imagelink.css'
 const ImageLink = ({onInputChange, onButtonSubmit}) => {
     return(
         <div>
-            <p className='f3 main_text'>
+            <p className='main_text'>
                 {'Find Who is he/she! Try it.'}
             </p>
-            <div className='center'>
-                <div className='form center pa4 br3 shadow-5'>
-                    <input className='f4 pa2 w-70 center' type ='text' placeholder='Image URL' onChange ={onInputChange}/>
+            <div className='center res'>
+                <div className='form center pa3 br3 shadow-5'>
+                    <input className='f4 pa2 w-70 center' type ='text' placeholder='https:// image url' onChange ={onInputChange}/>
                     <button 
                     className='w-30 grow f4 link ph3 pv2 div white bg-light-purple'
                     onClick = {onButtonSubmit}
