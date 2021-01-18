15c09db0878727ed1b22f849b0fdd550e1c6a4e5
MD Ahasan Habib <ahasanh303@gmail.com>
2021-01-19 02:01:05 +0600

    nothing

--- src/components/Nav/Navigation.js
+++ src/components/Nav/Navigation.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import './nav.css';
 
 const Navigation = () => {
     return (
-        <nav style = {{display:'flex', justifyContent: 'flex-end'}}>
+        <nav className='nav_bar'>
             <p className ='f3 link dim black underline pa3 pointer'>Sign Out</p>
         </nav>
     );
